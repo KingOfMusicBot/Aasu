286a88990f30eaebd4f1e674c71c137cc6e59e6f
KingOfMusicBot <teamriya84@gmail.com>
2025-10-19 17:33:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ GIT_TOKEN = getenv(
 
 
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
-YT_API_KEY = "xbit_UH3H68KMZRQF1UJTSCGLYS"
+YT_API_KEY = "xbit_8WBCRMOOEWUI80HIRAIXVR"
 COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
 
 
